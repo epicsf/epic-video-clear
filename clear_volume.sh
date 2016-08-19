78dbb9864f790515276992d247932ad9090f405c
Todd Eichel <todd@toddeichel.com>
2016-08-18 21:13:11 -0700

    Check return code of format command and warn user if it failed.

--- clear_volume.sh
+++ clear_volume.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # for safety: https://sipb.mit.edu/doc/safe-shell/
-set -euf -o pipefail
+set -uf -o pipefail
 
 read -p "Enter the disk letter to clear: " -n 1 -r
 echo
@@ -14,6 +14,14 @@ echo
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
   echo "OK, doing nothing."
   exit
+fi
+
+diskutil reformat ${VOLUME_PATH}
+
+EXIT_CODE=$?
+if [[ $EXIT_CODE != 0 ]]; then
+  echo "====== WARNING: Disk not cleared ======"
+  exit $EXIT_CODE
 fi
 
 echo
